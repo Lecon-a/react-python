bba85c98b848115671373b6336d0f8c0a67c8ae7
Sunday P. Afolabi <spog4true@gmail.com>
2024-07-04 15:24:35 -0700

    create build folder

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
-# Update this file for deployment
-
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from config import create_db
 
@@ -8,6 +7,18 @@ app = Flask(__name__)
 CORS(app)
 db = create_db(app)
 
+frontend_folder = os.path.join(os.getcwd(), "..", "client", "dist")
+
+# serve the frontend
+@app.route('/', defaults={'filename': ''})
+@app.route('/<path:filename>')
+def serve_frontend(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(frontend_folder, filename)
+
+
+# api routes
 import routes
 
 with app.app_context():
